4221ac230aab930fbce0fa46208ff42534cf35f6
SARAF <ravi.saraf@capgemini.com>
2018-11-13 16:26:02 +0530

    NEW JENKINS CHECK

---
+++ NewJEN/src/main/java/com/example/NewJEN/NewJenApplication.java
@@ -0,0 +1,12 @@
+package com.example.NewJEN;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewJenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewJenApplication.class, args);
+	}
+}
